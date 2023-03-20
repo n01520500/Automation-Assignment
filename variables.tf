3c80f143025f39d56500d13ed55bd52cbc425798
n01520500 <106194137+n01520500@users.noreply.github.com>
2023-03-19 21:52:02 -0400

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "tags" {
-  type        = map(string)
-  default = {
-    Project        = "Automation Project – Assignment 1"
-    Name           = "kavya.sharma"
-    ExpirationDate = "2023-06-30"
-    Environment    = "Lab"
-  }
-}
