907f41b6ac6f16964a64050747a7e6d6b2217388
n01520500 <106194137+n01520500@users.noreply.github.com>
2023-03-20 00:56:21 -0400

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "N01520500-assignment1-RG"
-    storage_account_name = "tfstate0500"
-    container_name       = "tfstate"
-    key                  = "3MyEBaFzODC0uqxtmQioodouzgvFJ9NQ5VywtkIkN7rNmRtdZQRtporOQ2+7EOJuh7gX5D7YbSQl+AStTuH2/Q=="
-  }
-}
